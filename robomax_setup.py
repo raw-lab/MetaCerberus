8845323454851ae9c84c280d7e560cf7fb81f8fa
aoukthrilok <66785977+aoukthrilok@users.noreply.github.com>
2020-12-09 20:22:30 -0500

    Update robomax_setup.py

--- robomax_setup.py
+++ robomax_setup.py
@@ -101,7 +101,7 @@ def wrapper_download():
     os.mkdir(path3, access_rights)
     os.chdir(path3)
     git.Repo.clone_from(git_URL, path3, branch='master')
-    shutil.move(os.path.join(path_to_wrapper, 'Robomax_Wrapper_v8.py'), path)
+    shutil.move(os.path.join(path_to_wrapper, 'Robomax_Wrapper_v9.py'), path)
     shutil.rmtree(path3)
 
 if __name__ == "__wrapper_download__":
