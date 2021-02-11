74e35fd36cdda53adef0b0c7fa256fc562a3cab0
Thrilok Kumar Reddy Aouk <aoukthrilok@outlook.com>
2021-02-11 00:49:49 -0500

    updated PCA

--- bin/cerberus.py
+++ bin/cerberus.py
@@ -19,6 +19,32 @@ import argparse
 from plotly.offline import plot
 import webbrowser
 from threading import Timer
+
+
+import plotly.graph_objs as go
+from plotly.offline import plot
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.express as px
+from sklearn.decomposition import PCA
+from functools import reduce
+import matplotlib.pyplot as plt 
+# import plotly.graph_objs as go
+import glob
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+import os
+
+
+
+
+
+
+
+
+
 app = dash.Dash(__name__)
 def preprocess_data(path,file_name):
     file=file_name.split('/')[-1]
@@ -66,6 +92,8 @@ def preprocess_data(path,file_name):
     table_list.append([table,path,f_name])
     return table
 def new_visual():
+
+    
     layout1 = go.Layout(
         title="My Dash Graph",
         height=1500,
@@ -102,7 +130,127 @@ def new_visual():
         grid= dict(columns=2, rows=row+1),
         margin = dict(t=0, l=0, r=0, b=0)
     )
-    plot(fig2, filename=path+'/'+'sunburst_plot'+".html", auto_open=True)
+    plot(fig2, filename=path+'/'+'sunburst_plot'+".html", auto_open=False)
+
+
+    tables=[]
+    options_bar=[]
+    for i,t in enumerate(table_list):
+        table=t[0]
+        mn=t[1]
+        f_name=t[2]
+        FT=table[table['Type']=='Foam'].drop(['Type'],axis=1)
+        KO=table[table['Type']=='KO'].drop(['Type'],axis=1)
+        FT_main=FT.copy()
+        KO_main=KO.copy()
+        FT1=FT[FT['Layer']==1].sort_values(by=['Count'],ascending=False).head(10)
+        FT2=FT[FT['Layer']==2].sort_values(by=['Count'],ascending=False).head(10)
+        FT3=FT[FT['Layer']==3].sort_values(by=['Count'],ascending=False).head(10)
+        FT4=FT[FT['Layer']==4].sort_values(by=['Count'],ascending=False).head(10)
+
+        KO1=KO[KO['Layer']==1].sort_values(by=['Count'],ascending=False).head(10)
+        KO2=KO[KO['Layer']==2].sort_values(by=['Count'],ascending=False).head(10)
+        KO3=KO[KO['Layer']==3].sort_values(by=['Count'],ascending=False).head(10)
+        KO4=KO[KO['Layer']==4].sort_values(by=['Count'],ascending=False).head(10)
+        FT=pd.concat([FT1,FT2,FT3,FT4])
+        KO=pd.concat([KO1,KO2,KO3,KO4])
+        f_path=os.path.join(mn + os.sep,f_name+'_output.xlsx')
+        csv_path=os.path.join(mn + os.sep, f_name+'_output')
+        table.to_excel (f_path, index = False, header=True)
+        table.to_csv (csv_path, index = False, header=True)
+        tables.append([table,FT,FT_main,KO,KO_main])
+        options_bar.append({"label": t[2], "value": i})
+    app.layout = html.Div(
+    #     style={'backgroundColor': colors['background']},
+        children=[
+        html.H1("Robomax Outputs", style={'text-align': 'center'}),
+        dcc.Graph(id='sunburst', figure=fig2),
+
+        html.H3("Select Type and Layer from the below Dropdowns", style={'text-align': 'left'}),
+        dcc.Dropdown(id="slct_number",
+                    options=options_bar,
+                    multi=False,
+                    value=0,
+                    style={'width': "40%"}
+                    ),
+        dcc.Dropdown(id="slct_Name",
+                    options=[
+                        {"label": "Foam", "value": 'Foam'},
+                        {"label": "KO", "value": 'KO'}],
+                    multi=False,
+                    value='Foam',
+                    style={'width': "40%"}
+                    ),
+        dcc.Dropdown(id="slct_Layer",
+                    options=[
+                        {"label": "Layer1", "value": 1},
+                        {"label": "Layer2", "value": 2},
+                        {"label": "Layer3", "value": 3},
+                        {"label": "Layer4", "value": 4}],
+                    multi=False,
+                    value=1,
+                    style={'width': "40%"}
+                    ),
+        html.Div(id='output_container', children=[]),
+        html.Br(),
+    #     dcc.Graph(id='pie_chart', figure={}),
+        dcc.Graph(id='my_bee_map', figure={}),
+        html.Br(),
+
+    ])
+
+
+    # ------------------------------------------------------------------------------
+    # Connect the Plotly graphs with Dash Components
+    @app.callback(
+        [
+            Output(component_id='output_container', component_property='children'),
+    #      Output(component_id='pie_chart', component_property='figure'),
+        Output(component_id='my_bee_map', component_property='figure')],
+        [Input(component_id='slct_Name', component_property='value'),Input(component_id='slct_Layer', component_property='value'),Input(component_id='slct_number', component_property='value')]
+    )
+    def update_graph(x,y,z):
+        table_val=tables[z]
+        FT=table_val[1]
+        FT_main=table_val[2]
+        KO=table_val[3]
+        KO_main=table_val[4]
+        container = "The year chosen by user was: {} {}".format(x,y)
+        if x=='Foam':
+            dff=FT.copy()
+            dff_main=FT_main.copy()
+        
+        if x=='KO':
+            dff=KO.copy()
+            dff_main=KO_main.copy()
+        dff=dff[dff['Layer']==y]
+        dff_main=dff_main[dff_main['Layer']==y]
+    #     fig1 = go.Figure(data=[go.Pie(labels=dff_main['Name'], values=dff_main['Count'],hole=0.5)])
+        layout = go.Layout(
+            title="My Dash Graph",
+            height=700,
+    #         paper_bgcolor='rgba(0,0,0,0)',
+            plot_bgcolor='rgba(255,0,0,0)'
+            )
+        fig=go.Figure(
+            data=[go.Bar(y=dff['Count'],x=dff['Name'])],
+            layout=layout
+        )
+
+        
+        return container, fig
+
+
+
+    # app = dash.Dash(__name__) 
+
+    port = 8050 
+
+    def open_browser():
+        webbrowser.open_new("http://localhost:{}".format(port))
+
+    Timer(1, open_browser).start()
+    app.run_server(port=port)
 
 
 def output_visual(path,f_name,FT,KO,FT_main,KO_main,table):
@@ -170,7 +318,7 @@ def output_visual(path,f_name,FT,KO,FT_main,KO_main,table):
     app.layout = html.Div(
     #     style={'backgroundColor': colors['background']},
         children=[
-        html.H1("Cerberus Outputs", style={'text-align': 'center'}),
+        html.H1("Robomax Outputs", style={'text-align': 'center'}),
         dcc.Graph(id='sunburst', figure=fig2),
 
         html.H3("Select Type and Layer from the below Dropdowns", style={'text-align': 'left'}),
@@ -309,10 +457,10 @@ def visual(table_value):
 
 def get_args():
     version="1.0"
-    parser = argparse.ArgumentParser(description='Cerberus is used for versatile functional ontology assignments for metagenomes via HMM searching with environmental focus of shotgun meta-omics data')
+    parser = argparse.ArgumentParser(description='RoboMax is used for versatile functional ontology assignments for metagenomes via HMM searching with environmental focus of shotgun meta-omics data')
     parser.add_argument('-i', type=str, required=True, help='path to file or directory \n <accepted formats {.faa,.fna,.ffn,.rollup} , for visualisation : {.rollup }>')
     parser.add_argument('--version','-v', action='version',
-                        version='Cerberus: \n version: {} December 24th 2020'.format(version),
+                        version='Robomax: \n version: {} December 24th 2020'.format(version),
                         help='show the version number and exit')
     
     args = parser.parse_args()
@@ -352,13 +500,13 @@ def get_file_list(args):
 #     return fna_path
 
 def fastq_processing(fq_path, path, f_name,file):
-    trim_path=path+f_name+"_trim.fastq"
+    trim_path=path+'/'+f_name+"_trim.fastq"
     cmd1="fastp -i "+fq_path+" -o " + trim_path
-    # cmd2="fastqc "+trim_path
-    trim_fna=path+f_name+"_trim.fna"
+    cmd2="fastqc "+trim_path
+    trim_fna=path+'/'+f_name+"_trim.fna"
     cmd3="sed -n '1~4s/^@/>/p;2~4p' "+trim_path+"  > "+trim_fna
     subprocess.call(cmd1,shell=True)
-    # subprocess.call(cmd2,shell=True)
+    subprocess.call(cmd2,shell=True)
     subprocess.call(cmd3,shell=True)
     return trim_fna
 
@@ -456,7 +604,72 @@ def faa_processing(faa_path,path,f_name):
     
     rollup_file = "%s.FOAM.out.sort.BH.KO.rollup" %(output_path)
     return roll_up(KO_ID_dict, rollup_file)
-
+def PCA1():
+#     m='/home/taouk/Desktop/robo/data/prokka_results_N139.ffn/N139_output/N139.FOAM.out.sort.BH.KO_output'
+#     z='/home/taouk/Desktop/robo/data/N139_output/N139.FOAM.out.sort.BH.KO_output'
+#     x='/home/taouk/Desktop/robo/data/input_dir/prokka_results_Agro_10k_R1.fastq/Agro_10k_R1_output/Agro_10k_R1.FOAM.out.sort.BH.KO_output'
+#     parent_dir = s
+#     y='/home/taouk/Desktop/robo/data/input_dir/prokka_results_Agro_10k_R2.fastq/Agro_10k_R2_output/Agro_10k_R2.FOAM.out.sort.BH.KO_output'
+# #     subject_dirs=s
+#     subject_dirs = [os.path.join(parent_dir, dir) for dir in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, dir))]
+#     csv_files=[x,y,z,m]
+    filelist = []
+#     for dir in subject_dirs:
+#         csv_files = [os.path.join(dir, csv) for csv in os.listdir(dir) if os.path.isfile(os.path.join(dir, csv)) and csv.endswith('_OUTPUT.XLSX.csv')]
+    for file in table_list:
+        a=file[0]
+        y=file[2]
+        # base=os.path.basename(file)
+        # y=os.path.splitext(base)[0]
+        # y = y.replace('_nucleotide_summary','').replace('_protein_summary','')
+        a = a[['Name','Count']]
+        a = a.rename(columns={'Count':y })
+        filelist.append(a)
+            
+    print(filelist)
+    df_merged2 = reduce(lambda  left,right: pd.merge(left,right,on=['Name'],
+                                                how='outer'), filelist)
+    result=df_merged2.replace(np.nan, 0)
+    pivoted=result.T
+    res=pivoted.rename(columns=pivoted.iloc[0])
+    res1=res.drop(res.index[0])
+    pca = PCA(n_components=3,svd_solver='randomized')
+    X_train= pca.fit_transform(res1)
+    labels = {
+    str(i): f"PC {i+1} ({var:.1f}%)"
+    for i, var in enumerate(pca.explained_variance_ratio_ * 100)
+    }
+    fig = px.scatter_3d(
+        X_train, x=0, y=1, z=2, color=res1.index,
+        labels=labels
+    )
+    fig.update_layout({
+    'plot_bgcolor' : '#7f7f7f',
+    'paper_bgcolor': '#FFFFFF',
+    'paper_bgcolor': "rgba(0,0,0,0)",
+    'plot_bgcolor' : '#7f7f7f',
+    
+    })
+    fig.update_layout(scene = dict(
+                        xaxis = dict(
+                            backgroundcolor="rgb(255,255, 255)",
+                            gridcolor="black",
+                            showbackground=True,
+                            zerolinecolor="black",),
+                        yaxis = dict(
+                            backgroundcolor="rgb(255,255, 255)",
+                            gridcolor="black",
+                            showbackground=True,
+                            zerolinecolor="black"),
+                        zaxis = dict(
+                            backgroundcolor="rgb(255,255, 255)",
+                            gridcolor="black",
+                            showbackground=True,
+                            zerolinecolor="black",),),
+                    
+                    )
+    # fig.show()
+    plot(fig, filename=path+'/'+'PCA_plot'+".html", auto_open=True)
 def main(path, file):
     f_name, f_ext = os.path.splitext(file)
     f_name=f_name.split('.')[0]
@@ -501,5 +714,12 @@ if __name__ == "__main__":
         main(path, f)
     if len(table_list)==1:
         visual(table_list[0])
-    elif len(table_list)<=6:
+    elif len(table_list)<=3:
+        # new_visual()
+        # PCA1()
         new_visual()
+    elif len(table_list)<=6:
+        PCA1()
+        new_visual()
+    elif len(table_list)>6:
+        PCA1()
