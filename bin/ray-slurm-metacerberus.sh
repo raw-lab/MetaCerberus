d6e0f46956b47fadb7c6646e3e704b393a3939a9
Richard Allen White III <raw937@gmail.com>
2022-04-06 14:55:12 -0400

    Add files via upload

---
+++ bin/ray-slurm-metacerberus.sh
@@ -0,0 +1,35 @@
+### ray-slurm.sh ###
+# Script for initializing RAY Cluster
+
+# Getting the node names
+nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
+nodes_array=($nodes)
+
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+
+
+port=6379
+ip_head=$head_node_ip:$port
+export ip_head
+>&2 echo "IP Head: $ip_head"
+
+>&2 echo "Starting HEAD at $head_node"
+srun --nodes=1 --ntasks=1 -w "$head_node" \
+    ray start --head --node-ip-address="$head_node_ip" --port=$port \
+    --num-cpus 1 --block &
+sleep 5
+
+
+# number of nodes other than the head node
+worker_num=$((SLURM_JOB_NUM_NODES - 1))
+
+for ((i = 1; i <= worker_num; i++)); do
+    node_i=${nodes_array[$i]}
+    >&2 echo "Starting WORKER $i at $node_i"
+    srun --nodes=1 --ntasks=1 -w "$node_i" \
+        ray start --address "$ip_head" \
+        --num-cpus 1 --block &
+    sleep 5
+done
+
