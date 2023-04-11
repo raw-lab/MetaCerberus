0182ed1ce47f28ff0a6bd7734b52bbb4d26e5e21
Jose Figueroa <jlfiguer@uncc.edu>
2023-04-11 15:51:56 -0400

    Updated dependency version of grpcio to avoid ray errors

--- install_metacerberus.sh
+++ install_metacerberus.sh
@@ -8,9 +8,10 @@ ENV_NAME=metacerberus-dev
 eval "$(conda shell.bash hook)"
 
 # create the metacerberus environment in conda
-mamba create -n $ENV_NAME -y -c conda-forge -c bioconda python git grpcio fastqc flash2 fastp porechop bbmap prodigal hmmer ray-default ray-core ray-dashboard gitpython pandas plotly scikit-learn dominate python-kaleido configargparse psutil metaomestats
+mamba create -n $ENV_NAME -y -c conda-forge -c bioconda python grpcio'=1.43' git fastqc flash2 fastp porechop bbmap prodigal hmmer ray-default ray-core ray-tune ray-dashboard gitpython pandas plotly scikit-learn dominate python-kaleido configargparse psutil metaomestats
 
 conda activate $ENV_NAME
 
 pip install .
+
 metacerberus.py --setup
