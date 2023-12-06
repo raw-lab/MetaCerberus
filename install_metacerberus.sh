e631d066cf1ac8c713ed6a2b4b4f17ea7c356084
Jose Figueroa <jlfiguer@uncc.edu>
2023-12-06 16:07:50 -0500

    Added dependencies for phanotate

--- install_metacerberus.sh
+++ install_metacerberus.sh
@@ -8,7 +8,7 @@ ENV_NAME=metacerberus
 eval "$(conda shell.bash hook)"
 
 # create the metacerberus environment in conda
-mamba create -y -n $ENV_NAME -c conda-forge -c bioconda python'>=3.8' grpcio'=1.43' fastqc flash2 fastp porechop bbmap prodigal prodigal-gv hmmer ray-default ray-core ray-tune ray-dashboard pandas plotly scikit-learn dominate python-kaleido configargparse psutil metaomestats
+mamba create -y -n $ENV_NAME -c conda-forge -c bioconda python'>=3.8' grpcio'=1.43' fastqc flash2 fastp porechop bbmap prodigal prodigal-gv trnascan-se hmmer ray-default ray-core ray-tune ray-dashboard pandas plotly scikit-learn dominate python-kaleido configargparse psutil metaomestats
 
 conda activate $ENV_NAME
 
