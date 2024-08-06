55d9cb6e849fcd51f7b0378253a49900f763696e
Jose Figueroa <jlfiguer@uncc.edu>
2024-08-06 14:56:15 -0400

    Edited tests

--- run-test.sh
+++ run-test.sh
@@ -21,24 +21,24 @@ DBPATH=~/database/db-metacerberus
 #metacerberus.py --download COG CAZy --db-path $DBPATH
 #metacerberus.py --download --db-path $DBPATH
 
-#rm -r temp-results
-#command time metacerberus.py --phanotate data/five_genomes/RW1.fna --hmm COG --keep --dir-out temp-results --db-path $DBPATH --slurm-single
+rm -r temp-results
+command time metacerberus.py --prodigal data/five_genomes/GIC* --hmm KOFam_prokaryote --keep --dir-out temp-results --db-path $DBPATH --chunker 1 --cpus 10
 
 #rm -r temp-NfixDB
-#command time metacerberus.py --prodigal data/rhizobium_test/ --hmm temp-db/NFixDB.hmm.gz --dir-out temp-NfixDB --db-path $DBPATH --slurm-single
+#command time metacerberus.py --prodigal data/rhizobium_test/ --hmm temp-db/NFixDB.hmm.gz --dir-out temp-NfixDB --db-path $DBPATH
 
 #rm -r temp-rhizobium
-#command time metacerberus.py --phanotate data/rhizobium_test/ --hmm COG --dir-out temp-rhizobium --slurm-single --class data/rhizobium_test/samples.tsv --db-path $DBPATH --slurm-single
+#command time metacerberus.py --phanotate data/rhizobium_test/ --hmm COG --dir-out temp-rhizobium --class data/rhizobium_test/samples.tsv --db-path $DBPATH
 
 #rm -r temp-GV
-#command time metacerberus.py --pyrodigalgv data/giantvirus.fna --hmm VOG --dir-out temp-GV --chunk 1 --db-path $DBPATH --slurm-single
+#command time metacerberus.py --pyrodigalgv data/giantvirus.fna --hmm VOG --dir-out temp-GV --chunk 1 --db-path $DBPATH
 
-rm -r temp-paired
-#--super ~/temp/raw-reads 
-#command time metacerberus.py --prodigalgv ~/temp/raw-reads --illumina --hmm COG --dir-out temp-paired --db-path $DBPATH --cpus 4
-
-args="--prodigalgv ~/temp/raw-reads --illumina --hmm COG --dir-out temp-paired --db-path $DBPATH"
-command time metacerberus.py $args --address "host" --cpus 4 &
-sleep 1
-command time metacerberus.py $args --address localhost --cpus 8 # &> client.log
-wait
+#rm -r temp-paired
+##--super ~/temp/raw-reads 
+##command time metacerberus.py --prodigal ~/temp/raw-reads --illumina --hmm KOFam_all COG --dir-out temp-paired --db-path $DBPATH --cpus 8
+#
+#args="--prodigalgv ~/temp/raw-reads --illumina --hmm COG --dir-out temp-paired --db-path $DBPATH"
+#command time metacerberus.py $args --address "host" --cpus 2 &
+#sleep 1
+#command time metacerberus.py $args --address localhost --cpus 8 # &> client.log
+#wait
